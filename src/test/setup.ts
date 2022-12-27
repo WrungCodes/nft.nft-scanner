c5d1c8f0f2b7f0c521e2b8af570ccf8aed2e2754
Daniel Fayemi <daniel.fayemi@latoken.com>
2022-12-27 23:33:05 +0100

    added mongo memory server for tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,11 +1,38 @@
-beforeAll(async () => {
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
 
+let mongo: any;
+
+beforeAll(async () => {
+    /**
+     * Initialize MongoDB Memory Server and save the instance so all tests can use it
+     */
+    mongo = new MongoMemoryServer();
+    await mongo.start();
+    const mongoUri = mongo.getUri();
+    
+    /**
+     * Connect to MongoDB Memory Server
+     */
+    await mongoose.connect(mongoUri);
 });
 
 beforeEach(async () => {
+    /**
+     * Get and delete all data in the DB after each test is run
+     */
+    const collections = await mongoose.connection.db.collections();
 
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
 });
 
 afterAll(async () => {
 
+    /**
+     * Stop MongoDB Memory Server and close Mongoose after the tests
+     */
+    await mongo.stop();
+    await mongoose.connection.close();
 });
