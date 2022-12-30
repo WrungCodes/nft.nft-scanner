82f3e0663444018f9689367c99f6247acd1264d7
Daniel Fayemi <daniel.fayemi@latoken.com>
2022-12-31 00:43:30 +0100

    refactored index and events

--- src/events/write-block.ts
+++ src/events/write-block.ts
@@ -1,34 +1,9 @@
 import Bull from "bull";
-import mongoose from "mongoose";
-import { Blockchain, BlockchainDoc } from "../models/blockchain";
+import { Blockchain } from "../models/blockchain";
 import { IBlockData } from "../blockchain/interfaces";
 
-console.log('Started')
-
-const queue = new Bull(
-    'write-block', 
-    process.env.REDIS ?? 'redis://127.0.0.1:6379', 
-);
-
-const erc721Queue = new Bull(
-    'send-erc721', 
-    process.env.REDIS ?? 'redis://127.0.0.1:6379', 
-);
-
-queue.process(async (job) => {
-    await connect( job );
-
-});
-
-let connected = false;
-
-const connect = async ( job: Bull.Job<IBlockData> ) => {
-    /**
-    * Get all enabled blockchains in the database so we can begin extracting
-    */
+export const writeBlock = async ( job: Bull.Job<IBlockData>, uploadAssetQueue: Bull.Queue<IBlockData> ) => {
     try {
-        if(connected == false){ connected = await init() }
-
         const bc = await Blockchain.findOne({ enabled: true, symbol: job.data.provider });
 
         if(bc == null) { throw new Error('invalid blockchain') }
@@ -40,42 +15,9 @@ const connect = async ( job: Bull.Job<IBlockData> ) => {
         bc.processedBlock = job.data.height
         bc.save()
 
-        erc721Queue.add(job.data, {  attempts: Infinity, backoff: 5000 })
+        uploadAssetQueue.add(job.data, {  attempts: Infinity, backoff: 5000 })
 
     } catch (err) {
         throw new Error(`Error ${err}`)
     }
-}
-
-const init = async () => {
-
-    /**
-     * This key is for mongodb database
-     */
-    if (!process.env.REDIS) {
-        throw new Error('REDIS_URI must be defined');
-    }
-
-    /**
-     * This key is for mongodb database
-     */
-    if (!process.env.MONGO_URI) {
-        throw new Error('MONGO_URI must be defined');
-    }
-
-    /**
-     * Here we try to connect to the mongodb database using the Key before procedding
-     * This would save some errors from occuring, mostly errors due to Invalid Mongo Credentials
-     * or Some Database unavalability
-     */
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('Connected to MongoDb');
-    } catch (err) {
-        console.error(err);
-    }
-
-    console.log('Connecting to DB')
-
-    return true
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { Blockchain, BlockchainDoc } from "./models/blockchain";
 import { Scanner } from "./services/scanner";
+import Bull from "bull";
+import { writeBlock } from "./events/write-block";
 
 const start = async () => {
     /**
@@ -18,6 +20,17 @@ const start = async () => {
     }
 
     /**
+     * This queue is where the the block add functionality is done.
+     */
+    const blockQueue = new Bull( 'write-block', process.env.REDIS );
+
+    /**
+     * This queue acts like a message queue for other services to recieve blockdata information scanned by 
+     * this service
+     */
+    const uploadAssetQueue = new Bull( 'upload-asset', process.env.REDIS );
+
+    /**
      * Here we try to connect to the mongodb database using the Key before procedding
      * This would save some errors from occuring, mostly errors due to Invalid Mongo Credentials
      * or Some Database unavalability
@@ -28,6 +41,13 @@ const start = async () => {
     } catch (err) {
         console.error(err);
     }
+
+    /**
+     * Worker to handle new block data mainly to increased processed blocks count in the database
+     */
+    blockQueue.process((job) => {
+        writeBlock( job, uploadAssetQueue );
+    });
 
     let blockchainsInDatabase: (BlockchainDoc & {_id: mongoose.Types.ObjectId; })[] = [];
 
